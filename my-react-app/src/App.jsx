af29d49c088e818d7af0ec96c5f4627bb9ed8038
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-03-15 14:34:45 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,7 @@
+import Welcome from "./components/Welcome"
+const App =()=>{
+  return(<div>
+    <Welcome/>
+  </div>)
+};
+export default App;
---
+++ my-react-app/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+const Welcome =()=>{
+  return(<div>
+    <h1>"Hello World"</h1>
+    <p>"Welcome to learning JSX!"</p>
+  </div>)
+};
+export default Welcome;
