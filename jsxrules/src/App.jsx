e7c89c2391b357e2f49c5f95f09eb16beefe9f33
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-03-18 15:25:02 +0530

    reactApp

--- jsxrules/src/App.jsx
+++ jsxrules/src/App.jsx
@@ -1,6 +1,12 @@
 import JsxRules from "./component/JsxRules"
+import Expression from "./expressionInReact/Expression";
 
 const App =()=>{
-  return <JsxRules/>
+return( 
+  <div>
+  <JsxRules/>
+  <Expression/>
+  </div>
+  );
 };
 export default App;
